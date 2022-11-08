c7a4be344c9614be1757f55e3930ec85619190cd
Isaiah Gordon <GordIsai857@GoTVDSB.ca>
2022-11-08 09:27:05 -0500

    tryed to add files

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
-﻿namespace Capin_Dread
+﻿using System.Text;
+using System.IO;
+
+namespace Capin_Dread
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int i=0;
+            List<string> list=new List<string>();
+            if (File.Exists("WordList.txt")||File.Exists("CapitalWordList.txt"))
+            {
+                foreach (string line in File.ReadLines(@"WordList.txt", Encoding.UTF8))
+                {
+                    i++;
+                    list.Add(line);
+                }
+                Console.WriteLine(i);
+                foreach (string line in list)
+                {
+                    File.WriteAllText(line.ToUpper(), @"CapitalWordList.txt");
+                }
+                if (File.ReferenceEquals(@"CapitalWordList.txt", @"WordList.txt".ToUpper()))
+                    Console.WriteLine("Succesfull");
+                else
+                    Console.WriteLine("Unsuccesfull");
+            }
+            else
+                Console.WriteLine("Files does not exist");
         }
+        
 
     }
 }
